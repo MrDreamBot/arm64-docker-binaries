d0a95e7d1ce95bcd99a18d28ae7d4ae964c885e0
mrdreambot <andyyuen@kardinia.com>
2016-10-01 08:34:16 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # arm64-docker-binaries
+
+For ODROID-C2 users using the Armbian Jessie server, docker.io (the docker engine) is not available in
+the repository meaning that you cannot apt-get install docker.
+
+I have made available the docker v1.12.1 binaries for you. The binaries have been tested on both ODROID-C2
+armbian xenial and jessie servers.
+
+The install.sh script requires root to run. You can simulate the installation and find out what commands
+it executes by changing to the arm64-docker-binaries directory and issue the command:
+
+sudo ./install.sh -s
+
+Once you are satisfied with it, issue the command:
+
+sudo ./install.sh
+
+When it finished, reboot. When it comes up, issue command:
+
+sudo docker info
+
+To avoid having to use sudo with each docker command, add group docker to your login as follows:
+
+sudo usermod -aG docker your_login
+
+Enjoy!
+
+
+
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ `whoami` != 'root' ]; then
+	echo You have to be root to run this script.
+	exit 1
+fi
+
+if [ $# -gt 0 ]; then
+
+	if [ $1 != "-s" ]; then
+		echo Invalid command line option: $1
+		echo Usage: $0 [-s]
+		exit 2
+	fi
+	echo Entering simulation mode...
+	ECHO=echo
+else
+	echo Starting installation...
+fi
+
+
+echo Copying docker binaries...
+$ECHO cp ./bin/docker /usr/bin/
+$ECHO cp ./bin/docker-containerd /usr/bin/
+$ECHO cp ./bin/docker-containerd-ctr /usr/bin/
+$ECHO cp ./bin/docker-containerd-shim /usr/bin/
+$ECHO cp ./bin/dockerd /usr/bin/
+$ECHO cp ./bin/docker-proxy /usr/bin/
+$ECHO cp ./bin/docker-runc /usr/bin/
+
+echo Checking existence of docker group
+if [ ! `grep docker /etc/group` ]; then
+        echo Adding docker group and adding root to the docker group...
+	$ECHO groupadd docker
+	$ECHO usermod -aG docker root
+
+	echo Setting up systemd
+	$ECHO cp docker.service /lib/systemd/system/
+	$ECHO ln -s /lib/systemd/system/docker.service /etc/systemd/system/multi-user.target.wants/docker.service
+	$ECHO cp docker /etc/init.d/
+else
+	echo docker group already exists, skipping set up of systemd docker.service
+fi
+
+echo When you are ready, reboot the system.
+
+docker.service can be controlled using the command:
+
+echo sudo systemctl start|sop docker.service
